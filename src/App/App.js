7371e76af6dc7cbce7caac37d60c5dec16f90997
Xin Xiong <xin.xiong1@thoughtworks.com>
2020-10-27 22:42:29 +0800

    <feat>: add button to add new member

--- src/App/App.js
+++ src/App/App.js
@@ -8,13 +8,12 @@ class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
-        <div className="MemberList">
-          学员列表
-          <div>
-            <MemberGrids className="Members">
-              <Member className="Member">member</Member>
-            </MemberGrids>
-          </div>
+        <div>
+          {/* eslint-disable-next-line react/button-has-type */}
+          <button className="DivisionButton">分组学员</button>
+          <MemberGrids className="MemberGrids">
+            <Member className="Member">member</Member>
+          </MemberGrids>
         </div>
       </div>
     );
