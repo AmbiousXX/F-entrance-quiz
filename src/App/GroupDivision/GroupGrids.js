7cdf81c98e4db3189b28d940bdef72cb54aee15b
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-11-02 02:10:07 +0800

    [mengqiu] add comments

--- src/App/GroupDivision/GroupGrids.js
+++ src/App/GroupDivision/GroupGrids.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './GroupGrids.css';
 
+// TODO GTB-工程实践: - 应该没有Grid这个维度，应该是List这个维度，所以就叫GroupList更合适一些
 class GroupGrids extends React.PureComponent {
   render() {
     return (
       <div>
+        {/* // TODO GTB-工程实践: - css class的格式规范应该是a-b这种格式 */}
         <p className="GroupsInfoTitle">分组列表</p>
       </div>
     );
--- src/App/Members/MemberGrids.js
+++ src/App/Members/MemberGrids.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './MemberGrids.css';
 
+// TODO GTB-工程实践: - 应该没有Grid这个维度，应该是List这个维度，所以就叫TraineeList更合适一些
 class MemberGrids extends React.PureComponent {
   render() {
     return (
       <div>
+          {/* // TODO GTB-工程实践: - css class的格式规范应该是a-b这种格式 */}
         <p className="MembersInfoTitle">学员列表</p>
         <button className="AddNewMemberButton" type="button">
           +添加学员
